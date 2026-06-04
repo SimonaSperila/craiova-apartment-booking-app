00e8c4f97d68ceb83259bf75d5e725e6458c8aeb
SimonaSperila <59610546+SimonaSperila@users.noreply.github.com>
2026-06-04 17:40:12 +0300

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import Logo from "./Logo";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
     const { t } = useTranslation();
@@ -9,24 +12,34 @@ function Footer() {
             <div className="container">
                 <div className="footer-top">
                     <Logo />
-                    <span className="vertical-border"></span>
                     <div className="contact-info">
-                        <p>{t('footer.contact')}</p>
-                        <p>Email: <a href="mailto:info@shakespearecentral.ro">info@shakespearecentral.ro</a></p>
+                        <p><FontAwesomeIcon icon={faLocationDot} /> Craiova, Romania</p>
+                        <p><FontAwesomeIcon icon={faPhone} /> <a href="tel:+40767813197">+40 767 813 197</a></p>
+                        <p><FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:info@shakespearecentral.ro">info@shakespearecentral.ro</a></p>
                     </div>
                     <div className="social-media">
-                        <a href="https://www.facebook.com/shakespearecentralapartment" target="_blank" rel="noopener noreferrer">Facebook</a>
-                        <a href="https://www.instagram.com/shakespearecentralapartment/" target="_blank" rel="noopener noreferrer">Instagram</a>
+                        <a href="https://www.facebook.com/profile.php?id=61585732636429" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                        <a href="https://www.instagram.com/shakespearecraiova/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
                     </div>
                 </div>
 
                 <div className="footer-bottom">
-                    <p>&copy; {new Date().getFullYear()} {t('footer.copyright')}</p>
-                    <p>{t('footer.rights')}</p>
+                    <div className="footer-copyright">
+                        <p>&copy; {new Date().getFullYear()} Shakespeare Central Apartment. {t('footer.copyright')}</p>
+                    </div>
+
+                    <div className="footer-links">
+                        <a href="/privacy">{t('footer.link1')}</a>
+                        <a href="/terms">{t('footer.link2')}</a>
+                    </div>
                 </div>
             </div>
         </footer>
     );
 }
 
-export default Footer 
+export default Footer;
